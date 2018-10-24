5e38102d5cb3f57594f0eff0c8f6a31771c7fd9b
CarolSum <905965287@qq.com>
2018-10-24 17:39:57 +0800

    add footer remain fields

--- src/components/share/footer/footer.tsx
+++ src/components/share/footer/footer.tsx
@@ -16,13 +16,30 @@ class Footer extends React.Component {
     return (
       <footer id='footer' className='wrapper menu'>
         <div className='height'>
-          <nav></nav>
+          <nav>
+            <a href='/' className='icseve-logo'>
+              <svg width='44px' height='62px'/>
+            </a>
+            <div className='content'>
+              <h2>Navigation</h2>
+              <ul>
+                <li><a href='/' className='fx-fill-center'>Home</a></li>
+                <li><a href='/' className='fx-fill'>Projects</a></li>
+                <li><a href='/' className='fx-fill'>Capabilities</a></li>
+                <li><a href='/' className='fx-fill'>About Me</a></li>
+              </ul>
+            </div>
+          </nav>
           <div className='contact'>
             <div className='content'>
               <h2>Contact</h2>
               <a href={`mailto: carolleesum@gmail.com`} className='fx-fill'>carolleesum@gmail.com</a>
               <br/>
               <a href='tel: +86 1234567890'>+86 1234567890</a>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
               <a href='#'>
                 <h3>School of Data and Computer Science</h3>
                 <br/>
@@ -30,7 +47,18 @@ class Footer extends React.Component {
               </a>
             </div>
           </div>
-          <div className='social'></div>
+          <div className='social'>
+            <div className='content'>
+              <h2>Let's keep in touch</h2>
+              <a href='https://www.github.com/carolsum'>
+                <svg width='44px' height='44px'></svg>
+              </a>
+              <br/>
+              <a href='https://twitter.com/905965287Sum'>
+                <svg width='44px' height='44px'></svg>
+              </a>
+            </div>
+          </div>
         </div>
       </footer>
     );
