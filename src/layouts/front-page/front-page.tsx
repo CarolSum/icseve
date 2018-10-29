7e64a435effd3d05db122fce3b0d4ec6e3273621
CarolSum <905965287@qq.com>
2018-10-30 00:03:35 +0800

    add frontpage framework

---
+++ src/layouts/front-page/front-page.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import './front-page.less';
+
+class FrontPage extends React.Component {
+  descriptionBoxStyle = {
+    width: '418px'
+  };
+
+  canvasStyle = {
+    width: '100vw',
+    height: '100vh',
+    cursor: 'inherit'
+  };
+
+  frontPageStyle: any = {
+    display: 'inherit',
+    visibility: 'visible'
+  };
+
+  public render() {
+    return (
+      <div className='site'>
+        <div id='main-patterns'>
+          <div className='pattern'>
+            <canvas style={this.canvasStyle}></canvas>
+          </div>
+        </div>
+        <div className='page frontpage' style={this.frontPageStyle}>
+          <div className='layer'>
+            <div className='part project intro'></div>
+            <div className='part project sigdetvidere'>
+              <div className='wrapper'>
+                <picture>
+                  <img src='../../assets/images/pass-it-on_desktop.jpg'
+                    sizes='(max-width: 299px) 66vw, 660px'
+                    alt='A picture of the Experimentarium Pass It On project'/>
+                </picture>
+              </div>
+            </div>
+          </div>
+          <div className='description-box' style={this.descriptionBoxStyle}>
+            <div className='wrapper'>
+              <div className='box intro'></div>
+              <div className='box pass-it-on'></div>
+              <div className='box cocio'></div>
+              <div className='box generous-store'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FrontPage;
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Footer from 'src/components/share/footer/footer';
+import FrontPage from 'src/layouts/front-page/front-page';
 import MenuButton from '../../components/share/menu-button/menu-button';
 import './home.less';
 
@@ -7,8 +8,8 @@ class TheHome extends React.Component {
   public render() {
     return (
       <div>
-        <h1>Home Page</h1>
         <MenuButton></MenuButton>
+        <FrontPage></FrontPage>
         <Footer></Footer>
       </div>
     );
